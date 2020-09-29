318f322d23516ef87b089c654024af7a3bab4be2
giovannimedici <giovannigilsu@gmail.com>
2020-09-29 15:07:29 -0300

    classe tabuleiro instanciada

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using xadrez.tabuleiro;
+using tabuleiro;
 
 namespace xadrez
 {
@@ -7,9 +7,8 @@ namespace xadrez
     {
         static void Main(string[] args)
         {
-            Posicao p;
-            p = new Posicao(3, 4);
-            Console.WriteLine(p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            
             Console.ReadLine();
         }
     }
