54c2734c3aceedbf2dee231572c6a5e92a501a39
giovannimedici <giovannigilsu@gmail.com>
2020-10-14 18:11:39 -0300

    jogada especial promocao

--- xadrez/xadrez/PartidaDeXadrez.cs
+++ xadrez/xadrez/PartidaDeXadrez.cs
@@ -131,6 +131,22 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Voce nao pode se  colocar em xeque...");
             }
+
+            Peca p = tab.peca(destino);
+
+            //jogada especial promocao
+            if(p is Peao)
+            {
+                if((p.cor == Cor.Branca && destino.linha ==0)|| (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
                 xeque = true;
             else
@@ -142,8 +158,6 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             // jogada especial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
